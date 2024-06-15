2076495436712b32b891d7f143f39b0b063de1b3
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-06-15 12:07:52 +0330

    feat: add simple number to storage

--- wrappers/FuncBlueprintTutorial1.compile.ts
+++ wrappers/FuncBlueprintTutorial1.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'func',
-    targets: ['contracts/func_blueprint_tutorial1.fc'],
+    targets: ['contracts/func_blueprint_tutorial_1.fc'],
 };
