600e400ba34b8420ebe5beadad99abd3e1256beb
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-06-15 12:12:15 +0330

    feat: update wrapper

--- wrappers/FuncBlueprintTutorial1.ts
+++ wrappers/FuncBlueprintTutorial1.ts
@@ -1,29 +1,39 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
-export type FuncBlueprintTutorial1Config = {};
+export type FuncBlueprintTutorial1Config = {
+    num: number;
+};
 
 export function funcBlueprintTutorial1ConfigToCell(config: FuncBlueprintTutorial1Config): Cell {
     return beginCell().endCell();
 }
 
 export class FuncBlueprintTutorial1 implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+    constructor(
+        readonly address: Address,
+        readonly init?: { code: Cell; data: Cell },
+    ) {}
 
-    static createFromAddress(address: Address) {
+    static createFromAddress(address: Address): FuncBlueprintTutorial1 {
         return new FuncBlueprintTutorial1(address);
     }
 
-    static createFromConfig(config: FuncBlueprintTutorial1Config, code: Cell, workchain = 0) {
+    static createFromConfig(config: FuncBlueprintTutorial1Config, code: Cell, workchain = 0): FuncBlueprintTutorial1 {
         const data = funcBlueprintTutorial1ConfigToCell(config);
         const init = { code, data };
         return new FuncBlueprintTutorial1(contractAddress(workchain, init), init);
     }
 
-    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint): Promise<void> {
         await provider.internal(via, {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async getNum(provider: ContractProvider): Promise<number> {
+        const result = await provider.get('get_num', []);
+        return result.stack.readNumber();
     }
 }
