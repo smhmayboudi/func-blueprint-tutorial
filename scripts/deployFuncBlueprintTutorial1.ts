495bacdd5765eefe1319db868b27d787a9298e67
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-06-15 15:49:27 +0330

    fix: run smart contract 1

--- scripts/deployFuncBlueprintTutorial1.ts
+++ scripts/deployFuncBlueprintTutorial1.ts
@@ -3,11 +3,17 @@ import { FuncBlueprintTutorial1 } from '../wrappers/FuncBlueprintTutorial1';
 import { compile, NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const funcBlueprintTutorial1 = provider.open(FuncBlueprintTutorial1.createFromConfig({}, await compile('FuncBlueprintTutorial1')));
-
+    const funcBlueprintTutorial1 = provider.open(
+        FuncBlueprintTutorial1.createFromConfig(
+            {
+                num: 10,
+            },
+            await compile('FuncBlueprintTutorial1'),
+        ),
+    );
     await funcBlueprintTutorial1.sendDeploy(provider.sender(), toNano('0.05'));
-
     await provider.waitForDeploy(funcBlueprintTutorial1.address);
-
     // run methods on `funcBlueprintTutorial1`
+    const num = await funcBlueprintTutorial1.getNum();
+    console.log('Num', num);
 }
